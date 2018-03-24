cfbc7602674b56e9989b2e33fce19f080ead5a5c
mohamed555hassan <37742620+mohamed555hassan@users.noreply.github.com>
2018-03-24 17:57:27 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int i;
+    cout<<"enter the number"<<endl;
+    cin>>x;
+    for(i=0 ;i<13 ;++i)
+        cout<<x*i<<endl;
+    return 0;
+}
